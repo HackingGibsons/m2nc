88917b40a4253b77c157e1f29da65be653454b9e
Sean Bryant <sean@vitrue.com>
2011-05-16 18:20:12 -0400

    [master] make sure the chunking functions know where to send the response.

--- src/m2nc.lisp
+++ src/m2nc.lisp
@@ -7,11 +7,11 @@
 
 (defun m2nc-input-handler (handler input-file-path content-type)
   (let ((request (m2cl:handler-receive handler)))
-        (m2cl:handler-send-http-chunked handler :headers `(("Content-Type" . ,content-type)))
+        (m2cl:handler-send-http-chunked handler :headers `(("Content-Type" . ,content-type)) :request request)
         (cond ((eql input-file-path *default-input*) (chunk-input handler request input-file-path))
               ((pathnamep input-file-path) (with-open-file (input-stream input-file-path)
                                              (chunk-input handler request input-stream))))
-        (m2cl:handler-send-http-chunked-finish handler)))
+        (m2cl:handler-send-http-chunked-finish handler :request request)))
 
 
 
