b1311d110a1fddcc5c32b8fc3e301f13330d49fe
Sean Bryant <sean@vitrue.com>
2011-05-16 18:00:21 -0400

    [master] make sure we send the response back to the requester.

--- src/m2nc.lisp
+++ src/m2nc.lisp
@@ -1,17 +1,17 @@
 (in-package :m2nc)
 
-(defun chunk-input (handler stream)
+(defun chunk-input (handler request stream)
   (loop for line = (read-line stream nil)
      until (eq line nil)
-     do (m2cl:handler-send-http-chunk handler line)))
+     do (m2cl:handler-send-http-chunk handler line :request request :binary-body-p t)))
 
 (defun m2nc-input-handler (handler input-file-path content-type)
-  (m2cl:handler-receive handler)
-  (m2cl:handler-send-http-chunked handler :headers `(("Content-Type" . ,content-type)))
-  (cond ((eql input-file-path *default-input*) (chunk-input handler input-file-path))
-        ((pathnamep input-file-path) (with-open-file (input-stream input-file-path)
-                                       (chunk-input handler input-stream))))
-  (m2cl:handler-send-http-chunked-finish handler))
+  (let ((request (m2cl:handler-receive handler)))
+        (m2cl:handler-send-http-chunked handler :headers `(("Content-Type" . ,content-type)))
+        (cond ((eql input-file-path *default-input*) (chunk-input handler request input-file-path))
+              ((pathnamep input-file-path) (with-open-file (input-stream input-file-path)
+                                             (chunk-input handler request input-stream))))
+        (m2cl:handler-send-http-chunked-finish handler)))
 
 
 
